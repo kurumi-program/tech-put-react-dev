9dcb8c622e07e65db448604305d36d323d61ca04
kurumi-program <kurumi.program@gmail.com>
2025-04-27 19:37:34 +0900

    Reactのビルド設定と環境変数の設定を追加

--- src/js/services/client.ts
+++ src/js/services/client.ts
@@ -7,7 +7,7 @@ const options = {
 
 export const client = applyCaseMiddleware(
   axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: process.env.REACT_APP_BASE_URL,
     withCredentials: true,
   }),
   options,
